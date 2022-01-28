46cd3fb10f0538ca129b8a5c6bdeccdde1837aa7
ice <IceIsHere00@gmail.com>
2022-01-28 12:04:29 +0330

    better input handling

--- build-AutoMake-Desktop_Qt_6_2_2_GCC_64bit-Debug/qtcsettings.cmake
+++
@@ -1,8 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/ice/Qt/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "PATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "/usr/bin/clang++-13" CACHE "STRING" "" FORCE)
-set("CMAKE_C_COMPILER" "/usr/bin/clang-13" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "/home/ice/Qt/6.2.2/gcc_64" CACHE "STRING" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/home/ice/Qt/6.2.2/gcc_64/bin/qmake" CACHE "STRING" "" FORCE)
